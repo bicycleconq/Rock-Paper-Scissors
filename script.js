ce231edd471c1db61716f58945257bb4331ed17c
bicycleconq <woinesmeskel@gmail.com>
2024-09-17 13:22:43 +0300

    Get Human Choice Initialize score value

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 console.log('Hello, World!');
-//declaration of code
-const rock = 'rock';
-const paper = 'paper';
-const scissor = 'scissor';
+//to get com choice 
 
-//to get com choice and return a string value
-function getComChoice (com) {
-    com = Math.floor(Math.random() * 10);
-    return com;  
+function getComputerChoice (rock, paper, scissor) {
+    let com = Math.floor(Math.random() * 3 + 1);
+    rock = 1;
+    paper = 2;
+    scissor = 3;
+     if (rock === com) {
+        return 'Rock';
+    } else if (paper === com) {
+        return 'Paper';
+    } else if (scissor === com) {
+        return'Scissors';
+    } 
 }
 
-console.log(getComChoice());
+// to get human/input choice
+
+function getHumanChoice (userInput) {
+    userInput = prompt('Choose from Rock, Paper, Scissor');
+    return 'You chose ' +  userInput;
+}
+
+
+
+//initial score value
+let computerScore = 0;
+let userScore = 0;
+
+//playing single round
+function playRound(computerChoice, humanChoice) {
+    humanChoice = userSelection;
+    humanChoice.toLowerCase();
+    computerChoice = comSelection;
+}
+
+const userSelection = getHumanChoice();
+const comSelection =  getComputerChoice();
+
+
+console.log(playRound(comSelection, userSelection));
